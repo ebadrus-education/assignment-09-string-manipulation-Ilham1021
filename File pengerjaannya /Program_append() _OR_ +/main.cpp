24317a5b69c3815bdf8a9a48245e76031b65759b
Ilham1021 <ilhamnashrullah5210@gmail.com>
2025-11-05 19:35:40 +0700

    Menggabungkan string

---
+++ File pengerjaannya /Program_append() _OR_ +/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string nama1 = "Muhammad Ilham Nashrullah";
+      string gabung = nama1 + " SMKN 1 Boyolangu ";
+    cout << "Hasil gabung: " << gabung << endl;
+
+return 0;
+}
