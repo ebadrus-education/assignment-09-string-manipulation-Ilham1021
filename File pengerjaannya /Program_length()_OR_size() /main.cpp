7166f351065e82807365c88a781f73bfa9bd17f9
Ilham1021 <ilhamnashrullah5210@gmail.com>
2025-11-05 19:29:26 +0700

    Menghitung panjang string

---
+++ File pengerjaannya /Program_length()_OR_size() /main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string nama1 = "Dwi Lestari";
+     cout << "panjang string: " << nama1.length() << endl;
+
+return 0;
+} 
