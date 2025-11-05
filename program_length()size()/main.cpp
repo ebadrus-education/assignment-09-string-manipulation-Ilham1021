5d705974655faf6ee0de97e4874492a3048b7b4f
Ilham1021 <ilhamnashrullah5210@gmail.com>
2025-11-05 19:10:23 +0700

    Delete program_length()size() directory

--- program_length()size()/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-using namespace std;
-int main() {
-    string nama1 = "Dwi Lestari";
-     cout << "panjang string: " << nama1.length() << endl;
-
-return 0;
-} 
