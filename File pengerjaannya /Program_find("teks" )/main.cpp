f2094418dac8e071a9aab820aca1dea07171852b
Ilham1021 <ilhamnashrullah5210@gmail.com>
2025-11-05 19:47:39 +0700

    Mencari posisi

---
+++ File pengerjaannya /Program_find("teks" )/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string nama1 = "Muhammad Ilham Nashrullah";
+          cout << "Posisi 'Ilham' di string: " << nama1.find("I") << endl;
+
+
+return 0;
+}
